fe6b25abd544863122a7f28be080d16b0cc89a93
ashok rajan <ashok@ashoks-MacBook-Pro.local>
2022-06-12 16:36:32 +0530

    feat: Add Game Won UI panel

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rigidbody2d;
+
+    public GameObject gameWonPanel;
     
     public float speed;
+
+    private bool isGameWon = false;
 
     // Update is called once per frame
     void Update()
     {
+        if(isGameWon == true)
+            return;
+
         if(Input.GetAxis("Horizontal") > 0) {
             rigidbody2d.velocity = new Vector2(speed, 0f);
         } else if(Input.GetAxis("Horizontal") < 0) {
@@ -25,7 +32,10 @@ public class PlayerController : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.tag == "Door")
+        if(other.tag == "Door") {
             Debug.Log("Level complete!!!");
+            isGameWon = true;
+            gameWonPanel.SetActive(true);
+        }
     }
 }
