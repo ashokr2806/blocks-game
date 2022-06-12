12a78cab7026e4eab23644cb133dee17bc15bf11
ashok rajan <ashok@ashoks-MacBook-Pro.local>
2022-06-12 20:13:20 +0530

    feat: Complete game loop and add enemy triggers

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rigidbody2d;
 
     public GameObject gameWonPanel;
+
+    public GameObject gameLostPanel;
     
     public float speed;
 
-    private bool isGameWon = false;
+    private bool isGameOver = false;
 
     // Update is called once per frame
     void Update()
     {
-        if(isGameWon == true)
+        if(isGameOver == true) {
+            rigidbody2d.velocity = new Vector2(0f, 0f);
             return;
+        }
 
         if(Input.GetAxis("Horizontal") > 0) {
             rigidbody2d.velocity = new Vector2(speed, 0f);
@@ -34,8 +39,17 @@ public class PlayerController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Door") {
             Debug.Log("Level complete!!!");
-            isGameWon = true;
+            isGameOver = true;
             gameWonPanel.SetActive(true);
+        } else if (other.tag == "Enemy") {
+            Debug.Log("Spike hit!!!");
+            gameLostPanel.SetActive(true);
+            isGameOver = true;
         }
+    } 
+
+    public void RestartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("Restart game...");
     }
 }
